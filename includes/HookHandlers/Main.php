23e603e1a8e4c45105a4438daf15e35ac72ec248
Tali64 <tali64squared@proton.me>
2026-07-01 19:29:42 +0000

    Fix stupid mistake

--- includes/HookHandlers/Main.php
+++ includes/HookHandlers/Main.php
@@ -56,7 +56,7 @@ class Main implements
     ImportDumpJobAfterImportHook,
     MessageCacheFetchOverridesHook,
     MimeMagicInitHook,
-    SiteNoticeAfterHook,
+    AfterHook,
     SkinAddFooterLinksHook,
     TitleReadWhitelistHook
 {
@@ -448,7 +448,7 @@ class Main implements
                 $siteNotice .= '<div class="wikitable" style="text-align: center; width: 90%; margin-left: auto; margin-right:auto; padding: 15px; border: 4px solid black; background-color: #EEE;"> <span class="plainlinks"> <img src="https://static.miraheze.org/metawiki/0/02/Wiki_lock.png" align="left" style="width:80px;height:90px;">' . $skin->msg( 'sitenotice-wiki-closed' )->parse() . '</span></div>';
             }
         } elseif ( $cwConfig->get( 'Inactive' ) && $cwConfig->get( 'Inactive' ) !== 'exempt' ) {
-            $siteNotice .= '<div class="wikitable" style="text-align: center; width: 90%; margin-left: auto; margin-right:auto; padding: 15px; border: 4px solid black; background-color: #EEE;"> <span class="plainlinks"> <img src="https://static.miraheze.org/metawiki/5/5f/Out_of_date_clock_icon.png" align="left" style="width:80px;height:90px;">' . $skin->msg( 'wiki-sitenotice-inactive' )->parse() . '</span></div>';
+            $siteNotice .= '<div class="wikitable" style="text-align: center; width: 90%; margin-left: auto; margin-right:auto; padding: 15px; border: 4px solid black; background-color: #EEE;"> <span class="plainlinks"> <img src="https://static.miraheze.org/metawiki/5/5f/Out_of_date_clock_icon.png" align="left" style="width:80px;height:90px;">' . $skin->msg( 'sitenotice-wiki-inactive' )->parse() . '</span></div>';
         }
     }
 
